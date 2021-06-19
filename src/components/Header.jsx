0bc486aba9f91e3934acf0c9834e067d05c156df
FriendGlak <camilojheans@gmail.com>
2021-06-18 21:10:56 -0500

    Navbar implementada con estilos.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
 
 const Header = ({ children }) => (
     <header>
-        <h1>Hola soy el header</h1>
         {children}
     </header>
 )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,86 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState, useEffect } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import { Button } from '../globalStyles';
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    NavIcon,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks,
+    NavBtnLink,
+    NavItemBtn
+} from '../style/NavbarStyles';
 
-const Navbar = () => (
-    <nav>
-        <h1>Hola soy el navbar</h1>
-    </nav>
-)
+
+
+const Navbar = () => {
+    const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click)
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+
+    useEffect(() => {
+        showButton();
+    }, [])
+
+    window.addEventListener('resize', showButton);
+    
+    return (
+        <>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <Nav>
+                    <NavbarContainer>
+                        <NavLogo to="/">
+                            <NavIcon />
+                            GLAK
+                        </NavLogo>
+                        <MobileIcon onClick={handleClick}>
+                            {click ? <FaTimes /> : <FaBars />}
+                        </MobileIcon>
+                        <NavMenu onClick={handleClick} click={click}>
+                            <NavItem>
+                                <NavLinks to='/'>Presentación</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to='/'>Proyectos</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to='/'>Testimonios</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to='/'>Contactos</NavLinks>
+                            </NavItem>
+                            <NavItemBtn>
+                                {button ? (
+                                    <NavBtnLink to="/curriculum">
+                                        <Button primary>Curriculum</Button>
+                                    </NavBtnLink>
+                                ) : (
+                                    <NavBtnLink to="/curriculum">
+                                        <Button fontBig primary>Curriculum</Button>
+                                    </NavBtnLink>
+                                )}
+                            </NavItemBtn>
+                        </NavMenu>
+                    </NavbarContainer>
+                </Nav>
+            </IconContext.Provider>
+        </>
+    )
+}
+
+
 
 export default Navbar;
