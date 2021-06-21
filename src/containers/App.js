2aa4aaf22f5fb0f28b01fe94dd84f814e69f3be8
FriendGlak <camilojheans@gmail.com>
2021-06-20 23:50:10 -0500

    HashRouter add.

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,26 +12,28 @@ import "../images/testimonial/testimonial3.png"
 import "../images/testimonial/testimonial4.png"
 import "../images/testimonial/testimonial5.png"
 import "../images/testimonial/testimonial6.png"
-import { BrowserRouter as Router, Switch, Route, Redirect, } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect, HashRouter } from 'react-router-dom';
 import Footer from '../components/Footer.jsx';
 
 
 const App = () => {
   return (
-    <Router>
-      <GlobalStyle />
-      <ScrollToTop />
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Redirect to="/inicio" component={Home} />
-        </Route>
-        <Route path='/inicio' exact component={Home} />
-        <Route path='/proyects' component={Proyect} />
-        <Route path='/testimonios' component={Testimonials} />
-      </Switch>
-      <Footer />
-    </Router >
+    <HashRouter basename="/">
+      <Router>
+        <GlobalStyle />
+        <ScrollToTop />
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/inicio" component={Home} />
+          </Route>
+          <Route path='/inicio' exact component={Home} />
+          <Route path='/proyects' component={Proyect} />
+          <Route path='/testimonios' component={Testimonials} />
+        </Switch>
+        <Footer />
+      </Router >
+    </HashRouter>
   );
 };
 
