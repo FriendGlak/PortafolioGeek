20ab6efe2209dc7b9619376fb72fc23d6e31d3ef
FriendGlak <camilojheans@gmail.com>
2021-06-20 19:50:48 -0500

    Sección "testimonios", añadida.

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,15 @@ import GlobalStyle from '../globalStyles.js';
 import Header from '../components/Header.jsx';
 import Navbar from '../components/Navbar.jsx';
 import Home from '../pages/Home/Home.js';
+import Testimonials from '../pages/Testimonials/Testimonials.js';
 import "../images/image1.png";
+import "../images/testimonial/testimonial1.png"
+import "../images/testimonial/testimonial2.png"
+import "../images/testimonial/testimonial3.png"
+import "../images/testimonial/testimonial4.png"
+import "../images/testimonial/testimonial5.png"
+import "../images/testimonial/testimonial6.png"
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Footer from '../components/Footer.jsx';
 
@@ -14,10 +22,12 @@ const App = () => {
       <GlobalStyle />
       <Header>
         <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch>
       </Header>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+      <Testimonials />
+
       <Footer />
     </Router>
   );
